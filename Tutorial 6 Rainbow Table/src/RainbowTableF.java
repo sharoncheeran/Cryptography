6d3db748977711a573f0a9a322c2fd2f9e9a9aa8
sharoncheeran <sharoncheeran@users.noreply.github.com>
2019-12-11 15:13:51 +0000

    With decrypt

--- Tutorial 6 Rainbow Table/src/RainbowTableF.java
+++ Tutorial 6 Rainbow Table/src/RainbowTableF.java
@@ -11,7 +11,6 @@ import java.util.Random;
 import java.math.BigInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.concurrent.ThreadLocalRandom; 
 /**
  *
  * @author sharo
@@ -22,23 +21,7 @@ public class RainbowTableF extends javax.swing.JFrame {
      * Creates new form RainbowTableF
      */
     public RainbowTableF() {
-        this.chainNumber = 100000000000;
         initComponents();
-    }
-    
-    public int Modulous(int x) //Modulous function
-    {
-        int y;
-        if (x < 0){
-            y = -x; //converting to positive - and - equals +
-            x = y % 11; //do mod
-            x = -x + 11; //converting the positive to negative number then adding 11
-        }
-        else
-        {
-            x = x % 11; //do this if integer already positive
-        }
-        return x;
     }
     
     private static String convertToHex(byte[] data) { //coverting to hex
@@ -68,30 +51,37 @@ public class RainbowTableF extends javax.swing.JFrame {
     } 
     
     String[] letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"};   //Storing letters into an array 
-    String pass = null;
+    String pass;
+    int sizePass = 4;
     int chainLength = 100;
-    int chainNumber;
-    HashMap<String, String> rainTable = new HashMap<>();
-    int sizePass = 101;
+    int numPass = 1727605; //36^4 + 36^3 + 36^2 + 36^1 + 36^0 + 36 is the total characters in the array
+    int chainNumber = numPass / chainLength;
+    HashMap <String, String> rainTable = new HashMap<>();
     Random rand = new Random();
     
     void rainTable()
     throws NoSuchAlgorithmException, UnsupportedEncodingException
     {
+        int count = 0;
         for(int i = 0; i < chainNumber; i++)
         {
-            String lastStr;
-            long firstN = rand.nextInt(sizePass);
-            //System.out.print(" " + firstN);
+            BigInteger firstN = new BigInteger (rand.nextInt(numPass) + "");
             String firstStr = intToString(firstN, letters);
-            lastStr = chainReduce(firstStr, 0);
+            String lastStr = chainReduce(firstStr, 0);
+            
+            if(rainTable.containsValue(lastStr))
+            {
+                System.out.println("Unusable " + firstStr);
+            }
+            
             if(rainTable.containsKey(lastStr))
             {
-                i++;
+                count++;
             }
             else
             {
-                rainTable.put(firstStr, lastStr);
+                rainTable.put(lastStr, firstStr);
+                System.out.println("LS: " + lastStr + "  " + "FS: " + firstStr);
             }
         }
     }
@@ -100,43 +90,86 @@ public class RainbowTableF extends javax.swing.JFrame {
     throws NoSuchAlgorithmException, UnsupportedEncodingException
     {
         BigInteger val = new BigInteger(hashes, 16);
-        long len = val.longValue();
+        String conversion = intToString(val, letters);
         
-        String conversion = intToString(len, letters);
         return conversion;
     }
     
     String intToString(BigInteger n, String[] alpha)
     {
-        BigInteger y = new BigInteger("0");
-        int xy;
+        BigInteger chr = new BigInteger("0");
         BigInteger base = new BigInteger(alpha.length + "");
+        int xy = 0;
         int num;
         String wrd = "";
-        while(n >= 0)
+        
+        while(n.compareTo(chr) != 0 && xy < sizePass)
         {
-            num = n % base;
-            n = n / base;
-            System.out.print(n + "\n");
+            num = n.mod(base).intValue();
+            n = n.divide(base);
             wrd = getLetter((int)num, letters) + wrd;
-            n = n - 1;
+            n = n.subtract(new BigInteger("1"));
+            xy++;
         }
+        
         return wrd;
     }
     
+    String Decrypt()
+    throws NoSuchAlgorithmException, UnsupportedEncodingException
+    {
+        int position = chainLength;
+        String userHash = jTextField1.getText().trim();
+        String userHash2 = jTextField1.getText().trim();
+        String result = reduceFunc(userHash, chainLength);
+        String start = rainTable.get(result);
+        System.out.println(userHash + "=======" + result);
+        
+        while(start == null && position > 0)
+        {
+            userHash = SHA1(start);
+            start = reduceFunc(userHash, chainLength);
+            position--;
+        }
+        
+        if (position == 0)
+        {
+            return null;
+        }
+        else
+        {
+            position = 0;
+            String newStart = start;
+            
+            while(position <= chainLength)
+            {
+                userHash = SHA1(newStart).trim();
+                
+                if(userHash.compareTo(userHash2) == 0)
+                {
+                    return newStart;
+                }
+                else
+                {
+                    position++;
+                    newStart = reduceFunc(userHash, position);
+                }
+            }
+            return null;
+        }
+    }
     String chainReduce(String hash, int pos) 
     throws NoSuchAlgorithmException, UnsupportedEncodingException
     {
-        String pwd;
         String hash1 = SHA1(hash);
-        pwd = reduceFunc(hash1, pos);
+        String pwd = reduceFunc(hash1, pos);
         while(pos != chainLength)
         {
             pos++;
             hash1 = SHA1(pwd);
             pwd = reduceFunc(hash1, pos);
         }
-        
+        System.out.println(pwd + "-------" + hash1);
         return pwd;
     }
     
@@ -155,6 +188,8 @@ public class RainbowTableF extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -172,25 +207,43 @@ public class RainbowTableF extends javax.swing.JFrame {
             }
         });
 
+        jButton3.setText("Decrypt");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jTextField1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)))
                 .addGap(6, 6, 6))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(163, 163, 163)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(40, 40, 40)
+                .addGap(14, 14, 14)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2))
-                .addContainerGap(36, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -207,6 +260,22 @@ public class RainbowTableF extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         System.exit(0);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        try {
+            String r = Decrypt();
+            if (r == null)
+            {
+                System.out.println("Unavailable");
+            }
+            else
+            {
+                System.out.println("Your password is:  " + r);
+            }
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            Logger.getLogger(RainbowTableF.class.getName()).log(Level.SEVERE, null, ex);
+        }    // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -246,5 +315,7 @@ public class RainbowTableF extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
